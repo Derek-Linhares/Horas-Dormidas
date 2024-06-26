0069ed78a2d4a33c6be4e69d44cb4ac8e7cfca14
Derek-Linhares <derikbruno3@gmail.com>
2024-06-26 05:43:05 -0300

    primeiro e provavelmente unico commit de um pequeno projeto com html, css e javascript onde calcula a quantidade de horas dormidas.

---
+++ script.js
@@ -0,0 +1,49 @@
+let bocejo = document.querySelector("#bocejo");
+
+function playSound(audio) {
+  audio.currentTime = 0;
+  audio.play();
+  audio.volume = 0.2;
+}
+
+function animar() {
+  let animacao = document.querySelector("#resultado");
+  animacao.style.color = "transparent";
+  animacao.style.textshadow = "none";
+  animacao.style.animation = "";
+  setTimeout(() => (animacao.style.animation = "go-surgir 5s  "), 1);
+  setTimeout(
+    () => (animacao.style.color = "black"),
+    (animacao.style.textshadow = "0.1rem 0.1rem #000000"),
+    5000
+  );
+}
+
+function calcular() {
+  let idade = document.querySelector("#idade").value;
+  let sono = document.querySelector("#sono").value;
+  let ronco = document.querySelector("#ronco");
+  let errorSound = document.querySelector("#erro");
+  let horasDeVida = (idade * 8760).toLocaleString("pt-BR");
+  let horasDormidas = (sono * 365 * idade).toLocaleString("pt-BR");
+  let horasDeVida2 = idade * 8760;
+  let horasDormidas2 = sono * 365 * idade;
+  let porcentagem = (horasDormidas2 / horasDeVida2) * 100;
+  let total = parseFloat(porcentagem.toFixed(2));
+  let resultado = document.querySelector("#resultado");
+  if (sono > 24) {
+    playSound(errorSound);
+
+    resultado.innerHTML = "As horas de sono não podem exceder 24 horas.";
+  } else if (idade === "" || sono === "") {
+    playSound(errorSound);
+
+    resultado.innerHTML = "Nenhum dos campos deve estar vazio.";
+  } else {
+    playSound(ronco);
+
+    resultado.innerHTML = `Você viveu até agora o total aproximado de ${horasDeVida} 
+        horas, nesse intervalo você dormiu por ${horasDormidas} horas!!! Isso é equivalente a passar 
+        ${total}% da sua vida dormindo.`;
+  }
+}
